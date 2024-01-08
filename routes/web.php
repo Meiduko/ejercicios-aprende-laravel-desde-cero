636408a844773be77a83a4c75b44b73d80998d8b
Borja González <borjago.bgo@gmail.com>
2024-01-08 14:38:51 +0100

    Ejercicio2

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use Spatie\LaravelIgnition\Support\StringComparator;
+
+use function PHPSTORM_META\type;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +31,60 @@ Route::get('/ejercicio1', function () {
 
 Route::post('/ejercicio1', function () {
     return "POST OK";
+});
+
+Route::put('/ejercicio1', function () {
+    return "PUT OK";
+});
+
+Route::patch('/ejercicio1', function () {
+    return "PATCH OK";
+});
+
+Route::delete('/ejercicio1', function () {
+    return "DELETE OK";
+});
+
+Route::post('/ejercicio2/a', function(Request $request) {
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $request->get('price')
+        ]
+    );
+});
+
+Route::post('/ejercicio2/b', function(Request $request) {
+    if($request->get('price') < 0){
+        return
+        Response::json(["message" => "Price can't be less than 0"], 422);
+    }
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $request->get('price')
+        ]
+    );
+});
+
+Route::post('/ejercicio2/c', function(Request $request) {
+    $price = $request->get('price');
+    $discount_code = ($request->get('discount'));
+    if ($discount_code == 'SAVE5'){
+        $discount = 5;
+    }elseif($discount_code == 'SAVE10'){
+        $discount = 10;
+    }elseif($discount_code == 'SAVE15'){
+        $discount = 15;
+    }else{
+        $discount = 0;
+    }
+    $price = $price-$price*($discount/100);
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $price,
+        'discount' => $discount
+        ]
+    );
 });
